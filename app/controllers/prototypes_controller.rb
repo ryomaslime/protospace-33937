c347b04547425850d981425fd781571894b61475
ryomaslime <77054799+ryomaslime@users.noreply.github.com>
2021-02-03 05:55:05 +0900

    after change

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -23,7 +23,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = Comment.all
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
